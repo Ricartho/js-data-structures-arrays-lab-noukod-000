8de95a7af27e81716b745810f5cc0756a8e2279d
Ricartho Franck <ricartho.franck@esih.edu>
2019-09-09 19:41:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution here!
-onst drivers = ["Milo", "Otis", "Garfield"];
+const drivers = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendDriver(name){
   drivers.push("Ralph");
